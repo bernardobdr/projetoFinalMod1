e13934554cb976aae982d08cc86e4e35c069e813
bernardobdr <bernardosilvabdr@gmail.com>
2021-11-22 22:11:46 -0300

    alterando texto heróis

---
+++ css/script.js
@@ -0,0 +1,18 @@
+function decidirOpcao() {
+  var escolha;
+  var opcaoUm = "1";
+  var opcaoDois = "2";
+
+  escolha = prompt("Decida o que fazer! Digite o número correspondente:\n1\n2");
+
+  if (escolha == "1") {
+    return (location = "http://127.0.0.1:5500/Caitlyn/faseTres.html");
+  } else if (escolha == "2") {
+    return (location = "http://127.0.0.1:5500/Caitlyn/gameoverFaseDois.html");
+  } else {
+    return alert("Número inválido");
+  }
+  return decidirOpcao;
+}
+
+setTimeout(decidirOpcao, 3000);
