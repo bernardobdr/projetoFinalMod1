85b693f6bb988122a28f56496e8240f65e1b927a
bernardobdr <bernardosilvabdr@gmail.com>
2021-11-23 03:29:18 -0300

    atualizando fases

--- silcoFase1.js
+++ silcoFase1.js
@@ -8,7 +8,7 @@ function decidirOpcao() {
   escolha = prompt("Decida o que fazer! Digite o número correspondente:\n1\n2");
 
   if (escolha == "1") {
-    return (location = "http://127.0.0.1:5501/silcoFase1.html");
+    return (location = "http://127.0.0.1:5501/silcoFase2.html");
   } else if (escolha == "2") {
     return (location = "http://127.0.0.1:5501/gameover.html");
   } else {
