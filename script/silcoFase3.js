7bdd71f1db59ea7015efd30fb10954b81ccdab81
bernardobdr <bernardosilvabdr@gmail.com>
2021-11-23 04:21:12 -0300

    criando outras fases

--- script/silcoFase3.js
+++ script/silcoFase3.js
@@ -1,5 +1,21 @@
 var mensagemBemVindo = alert("Parabéns! Você venceu.");
 
-function vencedor() {
-  return (location = "http://127.0.0.1:5501/silcoFaseFinal.html");
+function decidirOpcao() {
+  var escolha;
+  var opcaoUm = "1";
+  var opcaoDois = "2";
+
+  escolha = prompt(
+    "Decida o que fazer! Digite o número correspondente: 1 ou 2"
+  );
+
+  if (escolha == "1") {
+    return (location = "http://127.0.0.1:5501/silcoFaseFinal.html");
+  } else if (escolha == "2") {
+    return (location = "http://127.0.0.1:5501/gameover.html");
+  } else {
+    return alert("Número inválido");
+  }
+  return decidirOpcao;
 }
+setTimeout(decidirOpcao, 1000);
